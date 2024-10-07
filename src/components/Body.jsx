782ff86bd83d940467a26526d018131f80d9d814
vansh <vanshsin784@gmail.com>
2024-10-07 09:42:27 +0530

    Changes in Body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,40 +1,56 @@
 import Card from "./RestaurantCardList";
-// import { restaurantList } from "../Constants";
-import { useState,useEffect } from "react";
-import { filterData,CallAPI } from "../Constants";
-const Body=()=>{
-    const [searchText,setSearchText]=useState("");
-    const [search,setSearch]=useState([]);
-    useEffect(()=>{
-                const ans= CallAPI();
-                const restaurant =ans?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-                setSearch(restaurant);
-                console.log(search);
-    },[]);
+import { useState, useEffect } from "react";
+import { filterData, CallAPI } from "../Constants";
 
-    return(
+const Body = () => {
+  const [searchText, setSearchText] = useState("");
+  const [filteredRest, setFilteredRest] = useState([]);
+  const [allRestaurant, setAllRestaurant] = useState([]);
 
-        <>
-            <div className="Search-Box">
-                <input spellCheck="false" type="text" placeholder="Enter Search" id="Search-bar" value={searchText} onChange={(e)=>
-                    setSearchText(e.target.value)
-                }/>
-                <button onClick={()=>{
-                    const data=filterData(searchText,search);
-                    if(!data.ok) setSearch(data);
-                    else setSearch(search);
+  // Fetch data from API and update state
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      try {
+        const resp = await CallAPI();
+        const restaurants = resp?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        setAllRestaurant(restaurants);
+        setFilteredRest(restaurants);
+      } catch (error) {
+        console.error("Error fetching restaurants:", error);
+      }
+    };
 
-                }}><i className="fa fa-search" id="btn"></i></button>
-            </div>
-            <div className="Card-container">
-                {
-                    search.map((rest)=>{
-                        return <Card {...rest.info} key={rest.info.id}/>
-                    })
-                }
-            </div>
-        </>
+    fetchRestaurants();
+  }, []);
+  return (
+    <>
+      <div className="Search-Box">
+        <input
+          spellCheck="false"
+          type="text"
+          placeholder="Enter Search"
+          id="Search-bar"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            const data = filterData(searchText, allRestaurant);
+            data?setFilteredRest(data):setFilteredRest(allRestaurant);
 
-    )
-}
+          }}
+        >
+          <i className="fa fa-search" id="btn"></i>
+        </button>
+      </div>
+
+      <div className="Card-container">
+        {filteredRest.map((rest) => {
+          return <Card {...rest.info} key={rest.info.id} />;
+        })}
+      </div>
+    </>
+  );
+};
+
 export default Body;
